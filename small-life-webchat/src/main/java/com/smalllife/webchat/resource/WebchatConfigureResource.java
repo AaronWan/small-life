2f84dd201e1d4fb1e8749b053db6c91c4910deaa
wansong <wansong@firstshare.cn>
2017-03-27 23:07:43 +0800

    add WebchatMessageResource

--- small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatConfigureResource.java
+++ small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatConfigureResource.java
@@ -1,6 +1,5 @@
 package com.smalllife.webchat.resource;
 
-import com.google.common.collect.Lists;
 import com.smalllife.qq.AesException;
 import com.smalllife.qq.SHA1;
 import org.springframework.stereotype.Service;
@@ -10,8 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Aaron on 26/03/2017.
@@ -20,21 +17,17 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Service
 public class WebchatConfigureResource {
-    @Path("configure")
+    @Path("/")
     @GET
-    public boolean configure(@QueryParam("signature") String signature,
-                     @QueryParam("echostr") String echostr,
-                     @QueryParam("timestamp") String timestamp,
-                     @QueryParam("nonce") String nonce) throws AesException {
-        String token="kuailedashan";
-        String aesKey="gxrdYpX1oyDgZdEpqWKdScmT3VPNKDBk4iAssxM66Fe";
-        List<String> tokens= Lists.newArrayList(token,timestamp,nonce);
-        Collections.sort(tokens);
-        return SHA1.getSHA1(token,timestamp,nonce,aesKey).equals(signature);
-    }
-
-
-    public static void main(String[] args) {
-
+    public String configure(@QueryParam("signature") String signature,
+                            @QueryParam("echostr") String echostr,
+                            @QueryParam("timestamp") String timestamp,
+                            @QueryParam("nonce") String nonce) throws AesException {
+        String token = "kuailedashan";
+        if (SHA1.getSHA1(token, timestamp, nonce).equals(signature)) {
+            return echostr;
+        } else {
+            return null;
+        }
     }
 }
---
+++ small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
@@ -0,0 +1,34 @@
+package com.smalllife.webchat.resource;
+
+import com.smalllife.qq.AesException;
+import com.smalllife.qq.XMLParse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+
+/**
+ * Created by Aaron on 27/03/2017.
+ */
+@Service
+@Path("/webchat")
+@Slf4j
+public class WebchatMessageResource {
+    @Path("/")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String receiveMessage(@QueryParam("signature") String signature,
+                                 @QueryParam("openid") String openId,
+                                 @QueryParam("timestamp") String timestamp,
+                                 @QueryParam("nonce") String nonce,@RequestBody byte[] body) throws AesException {
+        Object[] content=XMLParse.extract(new String(body));
+        log.info(Arrays.asList(content).toString());
+        return null;
+    }
+}
