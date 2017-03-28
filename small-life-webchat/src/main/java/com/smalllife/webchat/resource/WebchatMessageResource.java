2acc4daa6ae36757934f9026132556efc9cc8fc8
wansong <wansong@firstshare.cn>
2017-03-28 16:25:16 +0800

    update import

--- small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
+++ small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
@@ -1,24 +1,13 @@
 package com.smalllife.webchat.resource;
 
 import com.smalllife.qq.AesException;
-import com.smalllife.qq.XMLParse;
 import com.smalllife.qq.model.BaseMsg;
 import com.smalllife.qq.model.TextMsg;
 import lombok.extern.slf4j.Slf4j;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.io.InputStream;
-import java.util.*;
-
-import static sun.jvm.hotspot.debugger.win32.coff.DebugVC50X86RegisterEnums.TAG;
 
 /**
  * Created by Aaron on 27/03/2017.
