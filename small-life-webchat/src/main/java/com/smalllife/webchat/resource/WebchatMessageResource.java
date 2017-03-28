0f11b178c34a93a84a9a8d360ed714f0f12a1c68
wansong <Aaron@123>
2017-03-28 23:11:22 +0800

    update

--- small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
+++ small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
@@ -19,7 +19,7 @@ public class WebchatMessageResource {
     @Path("/")
     @POST
     @Produces(MediaType.APPLICATION_XML)
-    @Consumes(MediaType.APPLICATION_XML)
+    @Consumes(MediaType.TEXT_PLAIN)
     public BaseMsg receiveMessage(@QueryParam("signature") String signature,
                                  @QueryParam("openid") String openId,
                                  @QueryParam("timestamp") String timestamp,
