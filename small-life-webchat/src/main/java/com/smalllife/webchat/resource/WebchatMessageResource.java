35f87782addee593ba46feae7ed4e91e376ce497
wansong <Aaron@123>
2017-03-29 08:03:03 +0800

    update

--- small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
+++ small-life-webchat/src/main/java/com/smalllife/webchat/resource/WebchatMessageResource.java
@@ -1,6 +1,5 @@
 package com.smalllife.webchat.resource;
 
-import com.google.common.collect.Maps;
 import com.smalllife.qq.AesException;
 import com.smalllife.qq.model.BaseMsg;
 import com.smalllife.qq.model.TextMsg;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Map;
 
 /**
  * Created by Aaron on 27/03/2017.
@@ -22,14 +20,9 @@ public class WebchatMessageResource {
     @POST
     @Produces(MediaType.APPLICATION_XML)
     public BaseMsg receiveMessage(@QueryParam("signature") String signature,
-                                  @QueryParam("openid") String openId,
-                                  @QueryParam("timestamp") String timestamp,
-                                  @QueryParam("nonce") String nonce) {
-        Map<String, String> result = Maps.newHashMap();
-        result.put("openId", openId);
-        result.put("nonce", nonce);
-        TextMsg msg=new TextMsg();
-        msg.setContent(openId);
+                                 @QueryParam("openid") String openId,
+                                 @QueryParam("timestamp") String timestamp,
+                                 @QueryParam("nonce") String nonce, @BeanParam TextMsg msg)  {
         return msg;
     }
 
