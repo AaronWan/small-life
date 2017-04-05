ff5e93cac55ac1e4cb43511fcc269d5a76a46ecc
wansong <Aaron@123>
2017-04-05 22:16:07 +0800

    update

--- small-life-data/src/main/java/com/smalllife/dao/model/AppEntity.java
+++ small-life-data/src/main/java/com/smalllife/dao/model/AppEntity.java
@@ -2,6 +2,7 @@ package com.smalllife.dao.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Property;
