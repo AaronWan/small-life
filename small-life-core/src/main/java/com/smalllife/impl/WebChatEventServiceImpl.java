06dfd9669469a097e67a4305d8b768e9a9ba62a4
wansong <wansong@firstshare.cn>
2017-04-05 13:31:33 +0800

    update

--- small-life-core/src/main/java/com/smalllife/impl/WebChatEventServiceImpl.java
+++ small-life-core/src/main/java/com/smalllife/impl/WebChatEventServiceImpl.java
@@ -2,10 +2,12 @@ package com.smalllife.impl;
 
 import com.smalllife.WebChatEventService;
 import com.smalllife.model.WebChatMsg;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Aaron on 01/04/2017.
  */
+@Service
 public class WebChatEventServiceImpl implements WebChatEventService {
 
     @Override
--- small-life-core/src/main/java/com/smalllife/impl/WebChatMsgServiceImpl.java
+++ small-life-core/src/main/java/com/smalllife/impl/WebChatMsgServiceImpl.java
@@ -2,10 +2,12 @@ package com.smalllife.impl;
 
 import com.smalllife.WebChatMsgService;
 import com.smalllife.dao.model.SessionEntity;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Aaron on 03/04/2017.
  */
+@Service
 public class WebChatMsgServiceImpl implements WebChatMsgService {
     @Override
     public void sendTextMsg(SessionEntity session, String text) {
